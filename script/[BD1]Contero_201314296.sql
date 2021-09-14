dc0c408a9868df3af8ff3df3da61d814e84389f4
Hector_Orozco <36681061+hr9457@users.noreply.github.com>
2021-09-13 21:32:09 -0600

    Add files via upload

--- script/[BD1]Contero_201314296.sql
+++ script/[BD1]Contero_201314296.sql
@@ -1,35 +1,35 @@
-SELECT COUNT(*) FROM pais -- 109
+SELECT 'pais', COUNT(*) FROM pais -- 109
     UNION ALL      
-SELECT COUNT(*) FROM ciudad -- 600
+SELECT 'ciudad', COUNT(*) FROM ciudad -- 600
     UNION ALL
-SELECT COUNT(*) FROM direccion -- 605
+SELECT 'direccion', COUNT(*) FROM direccion -- 605
     UNION ALL
-SELECT COUNT(*) FROM tienda -- 2
+SELECT 'tienda', COUNT(*) FROM tienda -- 2
     UNION ALL
-SELECT COUNT(*) FROM estado_actividad -- 2 
+SELECT 'estado_actividad', COUNT(*) FROM estado_actividad -- 2 
     UNION ALL
-SELECT COUNT(*) FROM cliente -- 599
+SELECT 'cliente', COUNT(*) FROM cliente -- 599
     UNION ALL
-SELECT COUNT(*) FROM clasificacion -- 5
+SELECT 'clasficacion', COUNT(*) FROM clasificacion -- 5
     UNION ALL
-SELECT COUNT(*) FROM categoria -- 16
+SELECT 'categoria', COUNT(*) FROM categoria -- 16
     UNION ALL
-SELECT COUNT(*) FROM actor -- 199
+SELECT 'actor', COUNT(*) FROM actor -- 199
     UNION ALL
-SELECT COUNT(*) FROM traduccion -- 6
+SELECT 'traduccion', COUNT(*) FROM traduccion -- 6
     UNION ALL
-SELECT COUNT(*) FROM pelicula -- 1000
+SELECT 'pelicula', COUNT(*) FROM pelicula -- 1000
     UNION ALL
-SELECT COUNT(*) FROM  pelicula_actor -- 5462
+SELECT 'pelicula_actor', COUNT(*) FROM  pelicula_actor -- 5462
     UNION ALL
-SELECT COUNT(*) FROM pelicula_categoria -- 1000
+SELECT 'pelicula_categoria', COUNT(*) FROM pelicula_categoria -- 1000
     UNION ALL
-SELECT COUNT(*) FROM pelicula_traduccion  -- 1000
+SELECT 'pelicula_traduccion', COUNT(*) FROM pelicula_traduccion  -- 1000
     UNION ALL
-SELECT COUNT(*) FROM inventario -- 1521
+SELECT 'inventario', COUNT(*) FROM inventario -- 1521
     UNION ALL
-SELECT COUNT(*) FROM tipo_empleado -- 2
+SELECT 'tipo_empleado', COUNT(*) FROM tipo_empleado -- 2
     UNION ALL
-SELECT COUNT(*) FROM empleado -- 2
+SELECT 'empleado', COUNT(*) FROM empleado -- 2
     UNION ALL
-SELECT COUNT(*) FROM renta; -- 16044
+SELECT 'renta', COUNT(*) FROM renta; -- 16044
